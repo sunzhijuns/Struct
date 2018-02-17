a51d808ed1622962512668a273f3c256a2e28474
szj_ubuntu_mach <2843359242@qq.com>
2018-02-18 00:10:20 +0800

    memory

---
+++ List.h
@@ -0,0 +1,15 @@
+//
+// Created by sunzhijun on 18-2-17.
+//
+
+#ifndef STRUCT_LIST_H
+#define STRUCT_LIST_H
+
+template <class Elem>
+class List{
+public:
+    virtual void clear() = 0;
+
+};
+
+#endif //STRUCT_LIST_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include <list>
+
+
+#define MAIN_CPP
+#ifdef MAIN_CPP
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
+#endif
